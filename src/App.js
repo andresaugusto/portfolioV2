a101f5bce3ce6bed1552efeff6c44b0336ba3866
andresaugusto <andres.augusto@icloud.com>
2021-07-09 13:04:14 -0400

    initialized react-app + scaffolded basic landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+//DEPS
+
+//STYLES
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{}}>
+      {/* AOM LOGO */}
+      <svg version="1.1" x="0px" y="0px" width="720px" height="864px" viewBox="0 0 720 864" enable-background="new 0 0 720 864" pointerEvents='visiblePainted'>
+        <g id="_x30_">
+          <a href="https://google.com">
+            <path id="O" fill='rgba(255, 166, 0, 0.0)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0-28.795-23.344-52.139-52.139-52.139c-28.796,0-52.139,23.344-52.139,52.139c0,28.796,23.343,52.139,52.139,52.139C340.213,596.037,363.557,572.694,363.557,543.898L363.557,543.898z"/>
+          </a>
+          <a href="">
+            <polygon id="M" fill='rgba(255, 166, 0, 0.75)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="500.904,601.17 435.105,342.457 380.664,431.123"/>
+          </a>
+          {/* <polygon id="A" fill='rgba(255, 166, 0, 0.0)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="512.859,648.176 207.14,648.176 207.14,215.823"/> */}
+          <a href="">
+            <path id="AO" display="inline" fill='rgba(255, 166, 0, 0.75)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0,28.796-23.344,52.139-52.139,52.139c-28.796,0-52.139-23.343-52.139-52.139c0-28.795,23.343-52.139,52.139-52.139C340.213,491.76,363.557,515.104,363.557,543.898L363.557,543.898zM207.14,215.823v432.353h305.719L207.14,215.823z"/>
+          </a>
+        </g>
+      </svg>
     </div>
   );
 }
