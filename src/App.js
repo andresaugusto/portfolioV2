7591e1413760b7aa604df9e7d3788fedcd510f74
andresaugusto <andres.augusto@icloud.com>
2021-07-10 12:49:51 -0400

    styling and pseudo-element addition

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@
 //STYLES
 import './App.css';
 
+
+const bgChange = () => {
+  document.body.style.backgroundColor = 'red'
+}
+
+
+
+
 function App() {
   return (
-    <div style={{}}>
-      {/* AOM LOGO */}
-      <svg version="1.1" x="0px" y="0px" width="720px" height="864px" viewBox="0 0 720 864" enable-background="new 0 0 720 864" pointerEvents='visiblePainted'>
+    <div style={{width: ""}}>
+      <svg id='aomLogo' version="1.1" x="0px" y="0px" width="720px" height="864px" viewBox="0 0 720 864" enable-background="new 0 0 720 864" pointerEvents='visiblePainted'>
         <g id="_x30_">
+          {/* <polygon id="A" fill='rgba(255, 166, 0, 0.0)' stroke="none" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="512.859,648.176 207.14,648.176 207.14,215.823"/> */}
           <a href="https://google.com">
-            <path id="O" fill='rgba(255, 166, 0, 0.0)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0-28.795-23.344-52.139-52.139-52.139c-28.796,0-52.139,23.344-52.139,52.139c0,28.796,23.343,52.139,52.139,52.139C340.213,596.037,363.557,572.694,363.557,543.898L363.557,543.898z"/>
+            <path id="O" class='path-letters' fill='rgba(255, 166, 0, 0.0)' stroke="none" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0-28.795-23.344-52.139-52.139-52.139c-28.796,0-52.139,23.344-52.139,52.139c0,28.796,23.343,52.139,52.139,52.139C340.213,596.037,363.557,572.694,363.557,543.898L363.557,543.898z"/>
           </a>
           <a href="">
-            <polygon id="M" fill='rgba(255, 166, 0, 0.75)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="500.904,601.17 435.105,342.457 380.664,431.123"/>
+            <polygon id="M" class='path-letters' fill='rgba(255, 166, 0, 0.0)' stroke="none" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="500.904,601.17 435.105,342.457 380.664,431.123"/>
           </a>
-          {/* <polygon id="A" fill='rgba(255, 166, 0, 0.0)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="512.859,648.176 207.14,648.176 207.14,215.823"/> */}
           <a href="">
-            <path id="AO" display="inline" fill='rgba(255, 166, 0, 0.75)' stroke="#000000" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0,28.796-23.344,52.139-52.139,52.139c-28.796,0-52.139-23.343-52.139-52.139c0-28.795,23.343-52.139,52.139-52.139C340.213,491.76,363.557,515.104,363.557,543.898L363.557,543.898zM207.14,215.823v432.353h305.719L207.14,215.823z"/>
+            <path id="AO" class='path-letters' display="inline" fill='rgba(255, 166, 0, 0.0)' stroke="none" stroke-width="0.3543" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M363.557,543.898c0,28.796-23.344,52.139-52.139,52.139c-28.796,0-52.139-23.343-52.139-52.139c0-28.795,23.343-52.139,52.139-52.139C340.213,491.76,363.557,515.104,363.557,543.898L363.557,543.898zM207.14,215.823v432.353h305.719L207.14,215.823z"/>
           </a>
         </g>
       </svg>
