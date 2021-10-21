3e6819c397d9bd4844d42eb660cb5542b2fa47d7
andresaugusto <andres.augusto@icloud.com>
2021-10-21 17:27:15 -0400

    added genMedia links to projects object

--- src/data.js
+++ src/data.js
@@ -50,128 +50,22 @@ export const projects = {
                         media: null
                     },
                 },
-                media: {}
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1Iinc94LrVwbunCHvZ84ST7zYlEy55Ynv/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1DHuICewJW9hwopvg77yvzod-RIquERyf/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/10TPL491BFXIIADRAWSeuxt5cCwVup7qx/view?usp=sharing",
+                    },
+                }
             },
             styleInfluencers: {
                 colors: {
                     primaryColor: "orange",
                     secondaryColor: "gray"
-                }
-            }
-        },
-        fire: {
-            info: {
-                title: "Fire",
-                year: "2020",
-                projectType: "Responsive Website",
-                featuredTechnologies: ["React.js", "Express", "Node.js", "MongoDB"],
-                roles: ["SCRUM Master", "Frontend Engineer"],
-                headline: null,
-                photoHero: null,
-                overview: null,
-                toolsAndMethods: null,
-                problem: null,
-                solution: null,
-                approach: {
-                    research: null,
-                    synthesis: null,
-                    design: null,
-                    delivery: null
-                },
-                prototype: null,
-                nextSteps: null,
-                feSpotlights: {
-                    one: {
-                        text: null,
-                        media: null
-                    },
-                    two: {
-                        text: null,
-                        media: null
-                    },
-                    three: {
-                        text: null,
-                        media: null
-                    },
-                },
-                uiSpotlights: {
-                    one: {
-                        text: null,
-                        media: null
-                    },
-                    two: {
-                        text: null,
-                        media: null
-                    },
-                    three: {
-                        text: null,
-                        media: null
-                    },
-                },
-                media: {}
-            },
-            styleInfluencers: {
-                colors: {
-                    primaryColor: "olive",
-                    secondaryColor: "purple"
-                }
-            }
-        },
-        simon: {
-            info: {
-                title: "sim[]n",
-                year: "2020",
-                projectType: "Responsive Website",
-                featuredTechnologies: ["JavaScript", "Photoshop"],
-                roles: ["Frontend Engineer", "UI Designer"],
-                headline: null,
-                photoHero: null,
-                overview: null,
-                toolsAndMethods: null,
-                problem: null,
-                solution: null,
-                approach: {
-                    research: null,
-                    synthesis: null,
-                    design: null,
-                    delivery: null
-                },
-                prototype: null,
-                nextSteps: null,
-                feSpotlights: {
-                    one: {
-                        text: null,
-                        media: null
-                    },
-                    two: {
-                        text: null,
-                        media: null
-                    },
-                    three: {
-                        text: null,
-                        media: null
-                    },
-                },
-                uiSpotlights: {
-                    one: {
-                        text: null,
-                        media: null
-                    },
-                    two: {
-                        text: null,
-                        media: null
-                    },
-                    three: {
-                        text: null,
-                        media: null
-                    },
-                },
-                media: {}
-            },
-            styleInfluencers: {
-                colors: {
-                    primaryColor: "#807a70",
-                    secondaryColor: "white"
                 }
             }
         },
@@ -224,12 +118,90 @@ export const projects = {
                         media: null
                     },
                 },
-                media: {}
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1hRsjoP-XunFxNlSsNWkU0R28OpMM44cJ/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1d_2qLYbBCWhcYRhe51CnQKmITYdnIuHR/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/1GiZTixYN34-Lf5f-MGUkCSr0Vx-9Ic8o/view?usp=sharing",
+                    },
+                }
             },
             styleInfluencers: {
                 colors: {
                     primaryColor: "maroon",
                     secondaryColor: "gold"
+                }
+            }
+        },
+        fire: {
+            info: {
+                title: "Fire",
+                year: "2020",
+                projectType: "Responsive Website",
+                featuredTechnologies: ["React.js", "Express", "Node.js", "MongoDB"],
+                roles: ["SCRUM Master", "Frontend Engineer"],
+                headline: null,
+                photoHero: null,
+                overview: null,
+                toolsAndMethods: null,
+                problem: null,
+                solution: null,
+                approach: {
+                    research: null,
+                    synthesis: null,
+                    design: null,
+                    delivery: null
+                },
+                prototype: null,
+                nextSteps: null,
+                feSpotlights: {
+                    one: {
+                        text: null,
+                        media: null
+                    },
+                    two: {
+                        text: null,
+                        media: null
+                    },
+                    three: {
+                        text: null,
+                        media: null
+                    },
+                },
+                uiSpotlights: {
+                    one: {
+                        text: null,
+                        media: null
+                    },
+                    two: {
+                        text: null,
+                        media: null
+                    },
+                    three: {
+                        text: null,
+                        media: null
+                    },
+                },
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1bkLg__sHxmwAK56WZ7NauOwN7l1kWRZt/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1SKwo2qePnGRdnbX1DCUamV18KyzctEP0/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/1gcu77pCpS67lROOn98-RPxOs2_ebhYh_/view?usp=sharing",
+                    },
+                }
+            },
+            styleInfluencers: {
+                colors: {
+                    primaryColor: "olive",
+                    secondaryColor: "purple"
                 }
             }
         },
@@ -282,12 +254,90 @@ export const projects = {
                         media: null
                     },
                 },
-                media: {}
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1FBM1_TVm92NzTLl3Ru5ciAsrwODqFkax/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1iyulQxEWzBJIxpF2PmU7MT8OLl84_Onr/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/12x9qTMJ-oYe4duoFfHEu5d0f054EXBGB/view?usp=sharing",
+                    },
+                }
             },
             styleInfluencers: {
                 colors: {
                     primaryColor: "rgb(164, 179, 119)",
                     secondaryColor: "0, 0, 0, 0.5"
+                }
+            }
+        },
+        simon: {
+            info: {
+                title: "sim[]n",
+                year: "2020",
+                projectType: "Responsive Website",
+                featuredTechnologies: ["JavaScript", "Photoshop"],
+                roles: ["Frontend Engineer", "UI Designer"],
+                headline: null,
+                photoHero: null,
+                overview: null,
+                toolsAndMethods: null,
+                problem: null,
+                solution: null,
+                approach: {
+                    research: null,
+                    synthesis: null,
+                    design: null,
+                    delivery: null
+                },
+                prototype: null,
+                nextSteps: null,
+                feSpotlights: {
+                    one: {
+                        text: null,
+                        media: null
+                    },
+                    two: {
+                        text: null,
+                        media: null
+                    },
+                    three: {
+                        text: null,
+                        media: null
+                    },
+                },
+                uiSpotlights: {
+                    one: {
+                        text: null,
+                        media: null
+                    },
+                    two: {
+                        text: null,
+                        media: null
+                    },
+                    three: {
+                        text: null,
+                        media: null
+                    },
+                },
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1NGswjAW-ltHxqCXSYpPsp8CxdLqo10Ey/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1TlrULDtqv3JHqBdWSYeRnMF0WG9K0sU0/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/1nKUsvDm10sGN3gK5ycGpL4Lqfy7xpnic/view?usp=sharing",
+                    },
+                }
+            },
+            styleInfluencers: {
+                colors: {
+                    primaryColor: "#807a70",
+                    secondaryColor: "white"
                 }
             }
         },
@@ -340,7 +390,17 @@ export const projects = {
                         media: null
                     },
                 },
-                media: {}
+                genMedia: {
+                    images: {
+                        mainWide: "https://drive.google.com/file/d/1NGswjAW-ltHxqCXSYpPsp8CxdLqo10Ey/view?usp=sharing",
+                        mainTablet: "",
+                        mainPhone: "",
+                    },
+                    videos: {
+                        mainWide1080pX4: "https://drive.google.com/file/d/1ejOHVpneiiyqel6OuM-5CCAVFvWB5ew3/view?usp=sharing",
+                        mainWide540pX4: "https://drive.google.com/file/d/1SKHRIU4-RjMJtB2QC-DZmm1GhjTRGYPv/view?usp=sharing",
+                    },
+                }
             },
             styleInfluencers: {
                 colors: {
